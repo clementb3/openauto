2faa6993cdce3cf05689b2e024a01182e0fd70a0
clement boulay <boulayclement2003@gmail.com>
2026-02-03 15:54:07 +0100

    remove useless file

--- CMakeFiles/autoapp.dir/autoapp_autogen/GBFAFXFCVO/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/autoapp.dir/autoapp_autogen/GBFAFXFCVO/qrc_resources.cpp.o: \
- /mnt/c/Users/boula/source/repos/openauto/autoapp_autogen/GBFAFXFCVO/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- CMakeFiles/autoapp.dir/src/autoapp/Projection/OMXVideoOutput.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/autoapp.dir/src/autoapp/Projection/OMXVideoOutput.cpp.o: \
- /mnt/c/Users/boula/source/repos/openauto/src/autoapp/Projection/OMXVideoOutput.cpp \
- /usr/include/stdc-predef.h
